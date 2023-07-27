a02c53985194f5ceae74a94fe33cfe874f9bbf62
deep9893 <deeppratapsingh0996@gmail.com>
2023-07-27 18:02:10 +0530

    data transformation done

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,15 +1,14 @@
-import sys, os
-import pickle
-from src.exception import CustomException
 from src.logger import logging
+from src.exception import CustomException
+import os, sys
+import pickle
 
-def save_object(file_path, obj):
+def save_obj(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
-        os.makedirs(dir_path, exist_ok= True)
+        os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            pickle.dump(obj,file_obj )
     except Exception as e:
         raise CustomException(e, sys)
